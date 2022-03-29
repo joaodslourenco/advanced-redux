a26f244958e718ba2b6372250916768051932877
Joao Lourenco <joaodslourenco@gmail.com>
2022-03-29 11:25:24 -0300

    action creator thunk

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,43 @@
 import Cart from './components/Cart/Cart'
 import Layout from './components/Layout/Layout'
 import Products from './components/Shop/Products'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import { useEffect } from 'react'
 
+import Notification from './components/UI/Notification'
+import { sendCartData } from './store/cart-slice'
+
+let isInitial = true
+
 function App() {
+  const dispatch = useDispatch()
   const showCart = useSelector(state => state.ui.cartIsVisible)
   const cart = useSelector(state => state.cart)
+  const notification = useSelector(state => state.ui.notification)
 
   useEffect(() => {
-    fetch('https://react-http-3d91a-default-rtdb.firebaseio.com/cart.json', {
-      method: 'PUT',
-      body: JSON.stringify(cart)
-    })
-  }, [cart])
+    if (isInitial) {
+      isInitial = false
+      return
+    }
+
+    dispatch(sendCartData(cart))
+  }, [cart, dispatch])
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </>
   )
 }
 
